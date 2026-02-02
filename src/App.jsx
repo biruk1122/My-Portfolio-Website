3d9702fbe96e7eb0609037e517afc4cacc6e0607
biruk1122 <birukfikadu29@gmail.com>
2026-02-02 17:18:19 +0300

    update the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainLayouts from './layouts/MainLayouts.jsx';
 import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Projects from './pages/Projects.jsx';
+import ProjectDetails from './pages/ProjectDetails.jsx';
+import Contact from './pages/Contact.jsx';
 
 function App() {
   return (
-    
-      <Routes>
-        <Route path="/*" element={<MainLayouts/>}>
-          <Route path="" element={<Home/>} />
-        </Route>
-      </Routes>
-    
-  )
+    <Routes>
+      <Route path="/*" element={<MainLayouts />}>
+        <Route path="" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="projects/:id" element={<ProjectDetails />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
